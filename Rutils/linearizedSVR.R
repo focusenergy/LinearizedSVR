419856507c44c9be1cc0ec561434502a3b5e5e1c
Harsha <harshav@windlogics.com>
2011-11-08 09:39:42 -0600

    svr train

---
+++ Rutils/linearizedSVR.R
@@ -0,0 +1,50 @@
+library(kernlab)
+library(LiblineaR)
+
+LinearizedSVRTrain <- function(X, Y, 
+                C = 1, epsilon = 0.01, nump = floor(sqrt(N)), 
+                ktype=rbfdot, kpar=list(sigma=sum(km$tot.withinss)/N)){
+  
+  N <- dim(X)[1]; D <- dim(X)[2]
+  tmp <- normalize(cbind(Y,X))
+  Xn <- tmp$Xn[,-1]
+  Yn <- tmp$Xn[,1]
+  km <- suppressWarnings(kmeans(Xn, centers=nump))
+  prototypes <- km$centers
+  kernel <- do.call(ktype, kpar)
+  
+  print(kpar$sigma)
+  
+  Xt <- kernelMatrix(kernel, Xn, prototypes)
+  print(dim(Xt))
+  
+  Xt0 <- cbind(Yn-epsilon, Xt)
+  Xt1 <- cbind(Yn+epsilon, Xt)
+  data <- rbind(Xt0, Xt1)
+  labels <- append(array(0, N), array(1, N))
+   
+  svc <- LiblineaR(data, labels, type=2, cost=C, bias = TRUE)
+  model <- list(W = svc$W, prototypes=prototypes, params=tmp$params, kernel=kernel)
+  return(model)
+}
+                                             
+LinearizedSVRPredict <- function(model, newdata){
+  tmp <- normalize(cbind(array(0, dim(newdata)[1]),newdata), model$params) #the zero array is because the params had the target also
+  Xn <- tmp$Xn[,-1]
+  Xt <- kernelMatrix(model$kernel, Xn, model$prototypes)
+  Xt <- cbind(Xt, array(1, dim(Xt)[1]))
+  wx.b <- Xt %*% model$W[-1] #all but the first weight
+  Y.hat <- (-wx.b / model$W[1])
+  Y.hat <- Y.hat *(model$params$MM[1]-model$params$mm[1]) + model$params$mm[1] #unnormalize predictions
+  return(Y.hat)
+}
+ 
+normalize <- function(X, params){
+  if (missing(params)){
+    params <- list(MM = apply(X, 2, max), mm = apply(X, 2, min))
+  }
+  Xn <- sweep(X, 2, params$mm)
+  Xn <- sweep(Xn, 2, (params$MM-params$mm), FUN="/")     
+  return(list(Xn=Xn, params=params))
+}
+                                             
