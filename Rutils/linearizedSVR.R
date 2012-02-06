f85bb2aa34711b52776c50690e7ecf42fc522188
Ken Williams <Ken.Williams@WindLogics.com>
2012-02-06 16:37:19 -0600

    Add some test examples

--- Rutils/linearizedSVR.R
+++ Rutils/linearizedSVR.R
@@ -63,11 +63,20 @@ normalize <- function(X, params){
   return(list(Xn=Xn, params=params))
 }
 
-##' Test code:
-##' dat <- data.frame(y=1, x1=rnorm(500,1), x2=rnorm(500,1))
-##' dat <- rbind(dat, data.frame(y=-1, x1=rnorm(500,-1), x2=rnorm(500,-1)))
-##' plot(x2 ~ x1, dat, col=c("red","green")[1+(res>0)])
+##' Test code (should convert to RUnit):
+##' dat <- data.frame(y=2, x1=rnorm(500,1), x2=rnorm(500,1))
+##' dat <- rbind(dat, data.frame(y=1, x1=rnorm(500,-1), x2=rnorm(500,-1)))
+##' plot(x2 ~ x1, dat, col=c("red","green")[dat$y])
 ##' mod <- LinearizedSVRTrain(X=as.matrix(dat[-1]), Y=dat$y, nump=6)
 ##' res <- predict(mod, newdata=as.matrix(dat[-1]))
 ##' table(res>0)
-##' plot(x2 ~ x1, dat, col=c("red","green")[dat$y/2+1.5])
+##' plot(x2 ~ x1, dat, col=c("red","green")[1+(res>0.5)])
+##'
+##' dat2 <- data.frame(y=0, x1=rnorm(500,0), x2=rnorm(500,3))
+##' dat2 <- rbind(dat2, data.frame(y=0, x1=rnorm(500,0), x2=rnorm(500,-3)))
+##' dat2$y <- 1+(dat2$x1 > 0)
+##' plot(x2 ~ x1, dat2, col=c("red","green")[dat2$y])
+##' mod2 <- LinearizedSVRTrain(X=as.matrix(dat2[-1]), Y=dat2$y, nump=4)
+##' res2 <- predict(mod2, newdata=as.matrix(dat2[-1]))
+##' table(res2>1.5)
+##' plot(x2 ~ x1, dat2, col=c("red","green")[1+(res2>1.5)])
